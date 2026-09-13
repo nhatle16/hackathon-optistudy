903d5c3bb68a8c55efbb54b3b6fc8de9bd2e2d56
Minh Nhat Le <nhatle.stu@gmail.com>
2026-09-13 16:18:48 -0600

    Create an agent and invoke programmatic interaction

--- src/agent.py
+++ src/agent.py
@@ -1,20 +1,28 @@
 import os
 from langchain_nebius import ChatNebius
+from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Initialize Nebius Chat Model
-chat = ChatNebius(
+model = ChatNebius(
     model="openai/gpt-oss-120b",
     temperature=0.3,     # agent thinks more straightforward, less creative
     max_tokens=300,
+    base_url="https://api.studio.nebius.ai/v1/",
+    api_key=os.getenv("NEBIUS_API_KEY"),
     reasoning_effort="low"  # skip internal inference
 )
 
-# Invoke the model
-messages = [HumanMessage(content="Explain briefly what is derivative?")]
-response = chat.invoke(messages)
+# Create an agent
+agent = create_agent(
+    model=model,
+)
 
-print(response.content)
+# Invoke the agent
+query = "Explain briefly what is derivative?"
+response = agent.invoke({"messages": [HumanMessage(content=query)]})
+
+print(response["messages"][-1].content)
